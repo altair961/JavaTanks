d03dae105984c47e9f9037fe9187daf2b50dc3e6
altair961 <altair89@list.ru>
2026-06-27 12:46:15 +0300

    Implement DisplayManager the old way

---
+++ JavaTanks/src/main/java/com/altair961/engineTester/MainGameLoop.java
@@ -0,0 +1,17 @@
+package com.altair961.engineTester;
+
+import com.altair961.renderEngine.DisplayManager;
+
+public class MainGameLoop {
+    public static void main(String[] args) {
+        DisplayManager.createDisplay();
+
+        while(!Display.isCloseRequested) {
+            // game logic
+            // render
+            DisplayManager.updateDisplay();;
+        }
+
+        DisplayManager.closeDisplay();
+    }
+}
---
+++ JavaTanks/src/main/java/com/altair961/renderEngine/DisplayManager.java
@@ -0,0 +1,37 @@
+package com.altair961.renderEngine;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.system.MemoryUtil;
+
+public class DisplayManager {
+    private static final int WIDTH = 1280;
+    private static final int HEIGHT = 720;
+    private static final int FPS_CAP = 120;
+
+    public static void createDisplay(){
+        ContextAttribs attribs = new ContextAttribs(3,2);
+        attribs.withForwardCompatible(true);
+        attribs.withProfileCore(true);
+
+        try {
+            Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+            Display.create(new PixelFormat(), attribs);
+            Display.setTitle("Our First Display!");
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+        }
+
+        GL11.glViewport(0, 0, WIDTH, HEIGHT);
+    }
+
+    public static void updateDisplay() {
+        Display.sync(FPS_CAP);
+        Display.update();
+    }
+
+    public static void closeDisplay() {
+        Display.destroy();
+    }
+}
