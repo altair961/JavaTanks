d788a9de0ca82f618386807aa8d140d5c1103e76
altair961 <altair89@list.ru>
2026-06-28 17:40:48 +0300

    Apply index based rendering to avoid sending duplicate vertices to the GPU

--- JavaTanks/src/main/java/com/altair961/engineTester/MainGameLoop.java
+++ JavaTanks/src/main/java/com/altair961/engineTester/MainGameLoop.java
@@ -12,19 +12,19 @@ public class MainGameLoop {
         Loader loader = new Loader();
         Renderer renderer = new Renderer();
 
-        // OpenGL expects vertices to be defined counter-clockwise by default
         float[] vertices = {
-                // Left bottom triangle
-                -0.5f, 0.5f, 0f,
-                -0.5f, -0.5f, 0f,
-                0.5f, -0.5f, 0f,
-                // Right top triangle
-                0.5f, -0.5f, 0f,
-                0.5f, 0.5f, 0f,
-                -0.5f, 0.5f, 0f
+                -0.5f, 0.5f, 0f,    //V0
+                -0.5f, -0.5f, 0f,   //V1
+                0.5f, -0.5f, 0f,    //V2
+                0.5f, 0.5f, 0f      //V3
         };
 
-        RawModel model = loader.loadToVAO(vertices);
+        int[] indices = {
+                0, 1, 3,    // Top left triangle (V0, V1, V3)
+                3, 1, 2     // Bottom right triangle (V3, V1, V2)
+        };
+
+        RawModel model = loader.loadToVAO(vertices, indices);
 
         while(!DisplayManager.isCloseRequested()) {
             renderer.prepare();
