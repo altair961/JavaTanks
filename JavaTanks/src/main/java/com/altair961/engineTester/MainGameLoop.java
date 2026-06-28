ce3b50e930df913b231cdd173657350cf0bc1bb3
altair961 <altair89@list.ru>
2026-06-28 15:05:47 +0300

    Render a quad

--- JavaTanks/src/main/java/com/altair961/engineTester/MainGameLoop.java
+++ JavaTanks/src/main/java/com/altair961/engineTester/MainGameLoop.java
@@ -1,17 +1,39 @@
 package com.altair961.engineTester;
 
 import com.altair961.renderEngine.DisplayManager;
+import com.altair961.renderEngine.Loader;
+import com.altair961.renderEngine.RawModel;
+import com.altair961.renderEngine.Renderer;
 
 public class MainGameLoop {
     public static void main(String[] args) {
         DisplayManager.createDisplay();
 
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
+
+        // OpenGL expects vertices to be defined counter-clockwise by default
+        float[] vertices = {
+                // Left bottom triangle
+                -0.5f, 0.5f, 0f,
+                -0.5f, -0.5f, 0f,
+                0.5f, -0.5f, 0f,
+                // Right top triangle
+                0.5f, -0.5f, 0f,
+                0.5f, 0.5f, 0f,
+                -0.5f, 0.5f, 0f
+        };
+
+        RawModel model = loader.loadToVAO(vertices);
+
         while(!DisplayManager.isCloseRequested()) {
+            renderer.prepare();
             // game logic
-            // render
+            renderer.render(model);
             DisplayManager.updateDisplay();;
         }
 
+        loader.cleanUP();
         DisplayManager.closeDisplay();
     }
 }
